37c76166823a91aa47cdd4517388bfb888cdce5b
Aristotel <64754639+paskalov-aris@users.noreply.github.com>
2020-09-08 23:10:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// Занесение переменных
+const score = document.querySelector('.score'),
+    start = document.querySelector('.start'),
+    gameArea = document.querySelector('.gameArea'),
+    car = document.createElement('div');
+
+car.classList.add('car');
+
+// Добавляем слушатели событий
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
+
+// Объект с кнопками "стрелочек"
+const keys = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowRight: false,
+    ArrowLeft: false,
+}
+
+// Настройки
+const setting = {
+    start: false,
+    score: 0,
+    speed: 3
+}
+
+// Функция запуска игры
+function startGame() {
+    start.classList.add('hide');
+    setting.start = true;
+    gameArea.appendChild(car);
+    console.log(gameArea);
+    requestAnimationFrame(playGame);
+}
+
+// Рекурсивная функция игры
+function playGame() {
+    console.log('play game');
+
+    if(setting.start === true) {
+        requestAnimationFrame(playGame);
+    }
+}
+
+// Функция старта движения
+function startRun(event) {
+    event.preventDefault();
+    keys[event.keys] = true;
+}
+
+// Функция конца движения
+function stopRun(event) {
+    event.preventDefault();
+    keys[event.keys] = false;
+}
